7a2009085a7e86f8bbaa1c99e4ee16c60bc0ce51
hhj <gudwn357@gmail.com>
2023-03-18 22:33:23 +0900

    test

--- src/main/java/ladder/Ladder.java
+++ src/main/java/ladder/Ladder.java
@@ -8,4 +8,10 @@ public class Ladder {
         rows = new int[row][numberOfPerson];
     }
 
+    public void drawline(Position position) {
+    }
+
+    public int run(int startLadderNum) {
+        return 0;
+    }
 }
