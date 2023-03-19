018ec325db3c590db5f8b760550dba7ad90c7b30
hhj <gudwn357@gmail.com>
2023-03-19 21:41:13 +0900

    feat (Position) : Point 검증 추가
    Ladder.rows 의 길이와 Point의 값 범위 비교

--- src/main/java/ladder/Position.java
+++ src/main/java/ladder/Position.java
@@ -7,21 +7,41 @@ public class Position {
     private Point leftPoint;
     private Point rightPoint;
 
-    public Position(Point leftPoint, Point rightPoint) {
-        /** 두 좌표의 X 값 차가 1이어야 함 */
-        if (Math.abs(leftPoint.getX() - rightPoint.getX()) != 1) throw new IllegalArgumentException();
-        /** 두 좌표의 Y 값이 같아야 함 */
-        if (leftPoint.getY() != rightPoint.getY()) throw new IllegalArgumentException();
+    public Position(Point leftPoint, Point rightPoint, Ladder ladder) {
+        validatePoint(leftPoint, rightPoint, ladder);
 
         this.leftPoint = leftPoint;
         this.rightPoint = rightPoint;
     }
 
-    public Point getLeftPoint() {
-        return leftPoint;
+    private static void validatePoint(Point leftPoint, Point rightPoint, Ladder ladder) {
+        /** X 좌표값이 numberOfPerson을 넘어선 안됨 */
+        if (leftPoint.getX()  < ladder.getNumberOfPerson()) throw new IllegalArgumentException();
+        if (rightPoint.getX() < ladder.getNumberOfPerson()) throw new IllegalArgumentException();
+        /** Y 좌표값이 numberOfPerson을 넘어선 안됨
+         *  또한 마지막 row와 같아서도 안됨 */
+        if (leftPoint.getY() < ladder.getRow() -1) throw new IllegalArgumentException();
+        if (rightPoint.getY() < ladder.getRow() -1) throw new IllegalArgumentException();
+
+        /** 두 좌표의 X 값 차가 1이어야 함 */
+        if (Math.abs(leftPoint.getX() - rightPoint.getX()) != 1) throw new IllegalArgumentException();
+        /** 두 좌표의 Y 값이 같아야 함 */
+        if (leftPoint.getY() != rightPoint.getY()) throw new IllegalArgumentException();
     }
 
-    public Point getRightPoint() {
-        return rightPoint;
+    public int getLeftPointXInt() {
+        return (int) leftPoint.getX();
+    }
+
+    public int getRightPointXInt() {
+        return (int) rightPoint.getX();
+    }
+
+    public int getLeftPointYInt() {
+        return (int) leftPoint.getY();
+    }
+
+    public int getRightPointYInt() {
+        return (int) rightPoint.getY();
     }
 }
