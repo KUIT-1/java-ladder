c7dc3689a0a1fdb61c8cc245516d6836b29f2405
jiyun <kelly574@naver.com>
2023-03-20 04:18:27 +0900

    test: Test run() with drawLine()
    drawLine() 실행 후 run() 테스트

--- src/test/java/ladder/LadderTest.java
+++ src/test/java/ladder/LadderTest.java
@@ -20,5 +20,26 @@ public class LadderTest {
         assertEquals(2, ladder.run(2));
     }
 
+    @Test
+    @DisplayName("Line 1개 추가 후 Run (이동 방향 : 좌)")
+    void runLeftWithLine(){
+        ladder.drawLine(2, 3);
+        assertEquals(4, ladder.run(3));
+    }
+
+    @Test
+    @DisplayName("Line 1개 추가 후 Run (이동 방향 : 우)")
+    void runRightWithLine(){
+        ladder.drawLine(2, 3);
+        assertEquals(3, ladder.run(4));
+    }
+
+    @Test
+    @DisplayName("Line 여러 개 추가 후 Run")
+    void runWithLines(){
+        ladder.drawLine(2, 3);
+        ladder.drawLine(3, 3);
+        assertEquals(4, ladder.run(4));
+    }
 
 }
