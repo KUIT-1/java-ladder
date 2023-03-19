1d095c26cae89a3d65ab80dec7f6a13ca6ff602c
jiyun <kelly574@naver.com>
2023-03-20 04:10:43 +0900

    feat: Create empty ladder
    초기의 빈 사다리 생성

--- src/main/java/ladder/Ladder.java
+++ src/main/java/ladder/Ladder.java
@@ -3,9 +3,18 @@ package ladder;
 public class Ladder {
 
     private final int[][] rows;
+    int height;
+    int numberOfPerson;
 
-    public Ladder(int row, int numberOfPerson) {
-        rows = new int[row][numberOfPerson];
+    public Ladder(int height, int numberOfPerson) {
+        this.height = height;
+        this.numberOfPerson = numberOfPerson;
+        rows = new int[height+1][numberOfPerson+1];
+
+    }
+
+    public int run(int startNumber) {
+        return startNumber;
     }
 
 }
