34224c9f0317fd1187ca68d726fac38af054a93f
hhj <gudwn357@gmail.com>
2023-03-19 21:52:03 +0900

    feat (Ladder) : create run(int)
    반복문과 switch 를 사용하여 ladderNum, col 값 변경

--- src/main/java/ladder/Ladder.java
+++ src/main/java/ladder/Ladder.java
@@ -13,6 +13,20 @@ public class Ladder {
         rows[position.getRightPointXInt()][position.getRightPointYInt()] = -1;
     }
 
+    public int run(int ladderNum) {
+        int col = 0;
+
+        while (col + 1 < rows.length) {
+            switch (rows[ladderNum][col]) {
+                case 1: ladderNum++; col++; break;
+                case -1: ladderNum--; col++; break;
+                case 0: col++; break;
+            }
+        }
+
+        return ladderNum;
+    }
+
     public int getNumberOfPerson() {
         return rows[0].length;
     }
