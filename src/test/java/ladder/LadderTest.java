16c4f66677f237648a3a53cf6806649ec77afbda
jiyun <kelly574@naver.com>
2023-03-20 04:07:57 +0900

    test: Test empty ladder
    빈 사다리인 경우 run() 테스트

--- src/test/java/ladder/LadderTest.java
+++ src/test/java/ladder/LadderTest.java
@@ -1,6 +1,24 @@
 package ladder;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class LadderTest {
+
+    static Ladder ladder;
+
+    @BeforeEach
+    void init(){
+        ladder = new Ladder(5, 4);
+    }
+
+    @Test
+    @DisplayName("초기 Run : Line x")
+    void runWithoutLine(){
+        assertEquals(2, ladder.run(2));
+    }
 
 
 }
