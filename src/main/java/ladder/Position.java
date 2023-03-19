2f07cb442c5d3018ef6a869de0945e028e40fc2c
hhj <gudwn357@gmail.com>
2023-03-19 23:53:25 +0900

    refactor (Position) : validatePoint() 수정
    배열을 착각하여 X, Y가 반대로 적용되어 이를 수정

--- src/main/java/ladder/Position.java
+++ src/main/java/ladder/Position.java
@@ -15,20 +15,20 @@ public class Position {
     }
 
     private static void validatePoint(Point leftPoint, Point rightPoint, Ladder ladder) {
-        /** X 좌표값이 numberOfPerson을 넘어선 안됨 */
-        if (leftPoint.getX()  > ladder.getNumberOfPerson()) throw new IllegalArgumentException();
-        if (rightPoint.getX() > ladder.getNumberOfPerson()) throw new IllegalArgumentException();
-        /** Y 좌표값이 numberOfPerson을 넘어선 안됨
+        /** Y 좌표값이 numberOfPerson을 넘어선 안됨 */
+        if (leftPoint.getY()  > ladder.getNumberOfPerson()) throw new IllegalArgumentException();
+        if (rightPoint.getY() > ladder.getNumberOfPerson()) throw new IllegalArgumentException();
+        /** X 좌표값이 row을 넘어선 안됨
          *  또한 마지막 row와 같아서도 안됨 */
-        if (leftPoint.getY() > ladder.getRow() -1) throw new IllegalArgumentException();
-        if (rightPoint.getY() > ladder.getRow() -1) throw new IllegalArgumentException();
+        if (leftPoint.getX() > ladder.getRow() -1) throw new IllegalArgumentException();
+        if (rightPoint.getX() > ladder.getRow() -1) throw new IllegalArgumentException();
 
-        /** rightPoint.X > leftPoint.X*/
-        if (rightPoint.getX() <= leftPoint.getX()) throw new IllegalArgumentException();
-        /** 두 좌표의 X 값 차가 1이어야 함 */
-        if (leftPoint.getX() - rightPoint.getX() != -1) throw new IllegalArgumentException();
-        /** 두 좌표의 Y 값이 같아야 함 */
-        if (leftPoint.getY() != rightPoint.getY()) throw new IllegalArgumentException();
+        /** rightPoint.Y > leftPoint.Y*/
+        if (rightPoint.getY() <= leftPoint.getY()) throw new IllegalArgumentException();
+        /** 두 좌표의 Y 값 차가 1이어야 함 */
+        if (leftPoint.getY() - rightPoint.getY() != -1) throw new IllegalArgumentException();
+        /** 두 좌표의 X 값이 같아야 함 */
+        if (leftPoint.getX() != rightPoint.getX()) throw new IllegalArgumentException();
     }
 
     public int getLeftPointXInt() {
