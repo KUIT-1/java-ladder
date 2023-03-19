15de787769809ac31e0a6f90dcdb696d05db2475
hhj <gudwn357@gmail.com>
2023-03-19 21:50:44 +0900

    feat (Ladder) : create drawLine(Position)
    Position 의 값에 따라 rows 에 값을 할당.
    1 -> 오른쪽으로 연결, -1 -> 왼쪽으로 연결

--- src/main/java/ladder/Ladder.java
+++ src/main/java/ladder/Ladder.java
@@ -4,8 +4,21 @@ public class Ladder {
 
     private final int[][] rows;
 
-    public Ladder(int row, int numberOfPerson) {
-        rows = new int[row][numberOfPerson];
+    public Ladder(int numberOfPerson, int row) {
+        rows = new int[numberOfPerson][row];
+    }
+
+    public void drawLine(Position position) {
+        rows[position.getLeftPointXInt()][position.getLeftPointYInt()] = 1;
+        rows[position.getRightPointXInt()][position.getRightPointYInt()] = -1;
+    }
+
+    public int getNumberOfPerson() {
+        return rows[0].length;
+    }
+
+    public int getRow() {
+        return rows.length;
     }
 
 }
