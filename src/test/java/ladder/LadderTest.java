5a386e9611083bfcf2870dd883a5d0982a8aab74
jiyun <kelly574@naver.com>
2023-03-20 05:13:28 +0900

    refactor: 비슷한 동작의 테스트 합치기
    비슷한 동작을 처리하는 테스트 구문을 하나의 단위 테스트로 수정

--- src/test/java/ladder/LadderTest.java
+++ src/test/java/ladder/LadderTest.java
@@ -21,17 +21,12 @@ public class LadderTest {
     }
 
     @Test
-    @DisplayName("Line 1개 추가 후 Run (이동 방향 : 좌)")
-    void runLeftWithLine(){
+    @DisplayName("Line 1개 추가 후 Run")
+    void runWithLine(){
         ladder.drawLine(2, 3);
         assertEquals(4, ladder.run(3));
-    }
-
-    @Test
-    @DisplayName("Line 1개 추가 후 Run (이동 방향 : 우)")
-    void runRightWithLine(){
-        ladder.drawLine(2, 3);
         assertEquals(3, ladder.run(4));
+
     }
 
     @Test
