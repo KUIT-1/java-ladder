28d7cb035395cf2a90673c31d43ffe6f91c9dc1a
jiyun <kelly574@naver.com>
2023-03-21 05:30:31 +0900

    feat: Implement class manages lines by Position
    각 지점의 Line을 관리하는 클래스
    addLineto__ : 좌 또는 우로 뻗는 line 추가
    checkDirection(state) : 현지점에서 뻗어있는 line들과 state 값을 바탕으로 다음 방향을 지정

---
+++ src/main/java/ladder/Position.java
@@ -0,0 +1,36 @@
+package ladder;
+
+public class Position {
+
+    private boolean left;
+    private boolean right;
+
+    public Position() {
+        this.left = false;
+        this.right = false;
+    }
+
+    public void addLinetoLeft(){
+        this.left = true;
+    }
+
+    public void addLinetoRight(){
+        this.right = true;
+    }
+
+    public int checkDirection(int state) {
+        if(state == 0 && this.right){ // gotoRight
+            return 1;
+        }
+        if(state == 0 && this.left){ // gotoLeft
+            return -1;
+        }
+        if(state == 1 && this.right){ // gotoRight
+            return 1;
+        }
+        if(state == -1 && this.left){ // gotoLeft
+            return -1;
+        }
+        return 0;
+    }
+}
