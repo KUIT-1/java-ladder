0957a722a253eaeaed2c679c70c8bbe610ac8651
akfrdma0125 <nogain1215@gmail.com>
2023-03-21 22:18:35 +0900

    init

--- src/main/java/ladder/Ladder.java
+++ src/main/java/ladder/Ladder.java
@@ -2,10 +2,48 @@ package ladder;
 
 public class Ladder {
 
-    private final int[][] rows;
+    int nOfRow = 0;
+    int nOfcol = 0;
+    private final int[][] rows; //사다리
 
+    //row : 사다리의 높이, numberofPerson : 사다리 줄의 수
     public Ladder(int row, int numberOfPerson) {
+        this.nOfRow = row - 1;
+        this.nOfcol = numberOfPerson - 1;
         rows = new int[row][numberOfPerson];
     }
+
+    public void drawLine(int row, int col){
+        invalidatePosition(row,col);
+        rows[row][col] = 1;
+        rows[row][col+1] = -1;
+    }
+
+    public int run(int nOfPerson){
+        invalidateNOfPerson(nOfPerson);
+        int current = nOfPerson;
+        for (int i = 0; i < nOfRow; i++) {
+            if(rows[i][current] == 1) current++;
+            else if(rows[i][current] == -1) current--;
+        }
+        return current;
+    }
+
+    /*
+    0번째 행 가능 , 마지막 행 불가능
+    0번째 열 가능 , 마지막 열 불가능
+     */
+    private void invalidatePosition(int row, int col){
+        if ((row<0)||(row>=nOfRow)||(col<0)||(col>=nOfcol)){
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void invalidateNOfPerson(int nOfPerson){
+        if ((nOfPerson<0)||(nOfPerson>nOfcol)){
+            throw new IllegalArgumentException();
+        }
+    }
+
 
 }
--- src/test/java/ladder/LadderTest.java
+++ src/test/java/ladder/LadderTest.java
@@ -1,6 +1,86 @@
 package ladder;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class LadderTest {
+    @Test
+    @DisplayName("drawLine의 범위 초과")
+    void drawLineValidate() {
+        //when
+        int numberOfPerson = 4;
+        int row = 3;
+        Ladder ladder = new Ladder(row, numberOfPerson);
 
+        //given
+        int givenRow = 4;
+        int givenCol = 1;
 
+        //then
+        assertThrows(IllegalArgumentException.class,()->ladder.drawLine(givenRow,givenCol));
+    }
+
+    @Test
+    @DisplayName("nOfPosition의 범위 초과")
+    void nOfPersonValidate() {
+        //when
+        int numberOfPerson = 4;
+        int row = 3;
+        Ladder ladder = new Ladder(row, numberOfPerson);
+
+        //given
+        int nOfPerson = 4;
+
+        //then
+        assertThrows(IllegalArgumentException.class,()->ladder.run(nOfPerson));
+
+        //given
+        int nOfPerson2 = -1;
+
+        //then
+        assertThrows(IllegalArgumentException.class,()->ladder.run(nOfPerson2));
+
+    }
+
+    @Test
+    @DisplayName("run 실행 확인")
+    void FourRowFourColWithLine() {
+        //when
+        int numberOfPerson = 4;
+        int row = 4;
+        Ladder ladder = new Ladder(row, numberOfPerson);
+
+        //given
+        ladder.drawLine(0,1);
+        ladder.drawLine(1,0);
+        ladder.drawLine(2,2);
+
+        int nthOfPerson = 0;
+        //then
+        assertEquals(1,ladder.run(nthOfPerson));
+
+        nthOfPerson = 3;
+
+        assertEquals(2,ladder.run(nthOfPerson));
+    }
+
+    @Test
+    @DisplayName("초기 사다리 실행 확인")
+    void ZeroLineWithLine() {
+        //when
+        int numberOfPerson = 4;
+        int row = 4;
+        Ladder ladder = new Ladder(row, numberOfPerson);
+
+        int nthOfPerson = 1;
+        //then
+        assertEquals(1,ladder.run(nthOfPerson));
+
+        nthOfPerson = 2;
+
+        assertEquals(2,ladder.run(nthOfPerson));
+    }
 }
