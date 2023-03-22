29cde000dec4c59ef743d29b3c833fd38d7f701d
akfrdma0125 <nogain1215@gmail.com>
2023-03-22 14:50:00 +0900

    week1 complete

--- src/main/java/ladder/Ladder.java
+++ src/main/java/ladder/Ladder.java
@@ -1,49 +1,30 @@
 package ladder;
 
 public class Ladder {
+    private final Row[] rows;
 
-    int nOfRow = 0;
-    int nOfcol = 0;
-    private final int[][] rows; //사다리
-
-    //row : 사다리의 높이, numberofPerson : 사다리 줄의 수
     public Ladder(int row, int numberOfPerson) {
-        this.nOfRow = row - 1;
-        this.nOfcol = numberOfPerson - 1;
-        rows = new int[row][numberOfPerson];
+        rows = new Row[row];
+        for (int i = 0; i < row; i++) {
+            rows[i] = new Row(numberOfPerson);
+        }
     }
 
     public void drawLine(int row, int col){
-        invalidatePosition(row,col);
-        rows[row][col] = 1;
-        rows[row][col+1] = -1;
+        validatePosition(row);
+        rows[row].drawLine(col);
     }
 
-    public int run(int nOfPerson){
-        invalidateNOfPerson(nOfPerson);
-        int current = nOfPerson;
-        for (int i = 0; i < nOfRow; i++) {
-            if(rows[i][current] == 1) current++;
-            else if(rows[i][current] == -1) current--;
+    public int run(int position){
+        for (int i = 0; i < rows.length; i++) {
+            position = rows[i].nextPosition(position);
         }
-        return current;
+        return position;
     }
 
-    /*
-    0번째 행 가능 , 마지막 행 불가능
-    0번째 열 가능 , 마지막 열 불가능
-     */
-    private void invalidatePosition(int row, int col){
-        if ((row<0)||(row>=nOfRow)||(col<0)||(col>=nOfcol)){
+    private void validatePosition(int row){
+        if ((row<0)||(row>=rows.length)){
             throw new IllegalArgumentException();
         }
     }
-
-    private void invalidateNOfPerson(int nOfPerson){
-        if ((nOfPerson<0)||(nOfPerson>nOfcol)){
-            throw new IllegalArgumentException();
-        }
-    }
-
-
 }
--- src/test/java/ladder/LadderTest.java
+++ src/test/java/ladder/LadderTest.java
@@ -4,50 +4,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class LadderTest {
     @Test
-    @DisplayName("drawLine의 범위 초과")
-    void drawLineValidate() {
-        //when
-        int numberOfPerson = 4;
-        int row = 3;
-        Ladder ladder = new Ladder(row, numberOfPerson);
-
-        //given
-        int givenRow = 4;
-        int givenCol = 1;
-
-        //then
-        assertThrows(IllegalArgumentException.class,()->ladder.drawLine(givenRow,givenCol));
-    }
-
-    @Test
-    @DisplayName("nOfPosition의 범위 초과")
-    void nOfPersonValidate() {
-        //when
-        int numberOfPerson = 4;
-        int row = 3;
-        Ladder ladder = new Ladder(row, numberOfPerson);
-
-        //given
-        int nOfPerson = 4;
-
-        //then
-        assertThrows(IllegalArgumentException.class,()->ladder.run(nOfPerson));
-
-        //given
-        int nOfPerson2 = -1;
-
-        //then
-        assertThrows(IllegalArgumentException.class,()->ladder.run(nOfPerson2));
-
-    }
-
-    @Test
     @DisplayName("run 실행 확인")
-    void FourRowFourColWithLine() {
+    void runSuccessWhenFourRowFourColFourLine() {
         //when
         int numberOfPerson = 4;
         int row = 4;
@@ -57,30 +18,31 @@ public class LadderTest {
         ladder.drawLine(0,1);
         ladder.drawLine(1,0);
         ladder.drawLine(2,2);
+        ladder.drawLine(3,0);
 
-        int nthOfPerson = 0;
+        int position = 0;
         //then
-        assertEquals(1,ladder.run(nthOfPerson));
+        assertEquals(0,ladder.run(position));
 
-        nthOfPerson = 3;
+        position = 3;
 
-        assertEquals(2,ladder.run(nthOfPerson));
+        assertEquals(2,ladder.run(position));
     }
 
     @Test
     @DisplayName("초기 사다리 실행 확인")
-    void ZeroLineWithLine() {
+    void runSuccessWhenFourRowFourColNoLine() {
         //when
         int numberOfPerson = 4;
         int row = 4;
         Ladder ladder = new Ladder(row, numberOfPerson);
 
-        int nthOfPerson = 1;
+        int position = 1;
         //then
-        assertEquals(1,ladder.run(nthOfPerson));
+        assertEquals(1,ladder.run(position));
 
-        nthOfPerson = 2;
+        position = 3;
 
-        assertEquals(2,ladder.run(nthOfPerson));
+        assertEquals(3,ladder.run(position));
     }
 }
