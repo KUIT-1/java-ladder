691a0ee1154846ae5a1e602860dbfba265f7f9fe
somin-jeong <81250561+somin-jeong@users.noreply.github.com>
2023-03-23 12:18:07 +0900

    second commit

--- src/main/java/ladder/Ladder.java
+++ src/main/java/ladder/Ladder.java
@@ -1,11 +1,28 @@
 package ladder;
 
-public class Ladder {
 
-    private final int[][] rows;
+public class Ladder {
+    Rows rows;
 
     public Ladder(int row, int numberOfPerson) {
-        rows = new int[row][numberOfPerson];
+        rows = new Rows(row, numberOfPerson);
+    }
+
+    public void drawLine(int row, int col){
+        rows.drawLine(row,col);
+    }
+
+    public int run(int num){
+        return rows.run(num);
+    }
+
+    public void print(){
+        for(int row = 0; row< rows.getNumofRow(); row++){
+            for(int col = 0; col< rows.getNumofPerson(); col++){
+                System.out.print(rows.getValue(row,col) + " ");
+            }
+            System.out.println();
+        }
     }
 
 }
