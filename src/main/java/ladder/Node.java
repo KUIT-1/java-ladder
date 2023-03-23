b8684cf857e303fd887896d50c3047b1b6d61d04
jiyun <kelly574@naver.com>
2023-03-24 06:14:56 +0900

    refactor: Rename field of Node Class

--- src/main/java/ladder/Node.java
+++ src/main/java/ladder/Node.java
@@ -2,35 +2,41 @@ package ladder;
 
 public class Node {
     private int state; // -1 : gotoLeft, 1 : gotoRight, 0 : gotoDown
-    private int row;
-    private int col;
+    private int current_row;
+    private int current_col;
 
     public Node(int startPoint){
         this.state = 0;
-        this.row = 1;
-        this.col = startPoint;
+        this.current_row = 1;
+        this.current_col = startPoint;
     }
 
-    public int move(PositionManager positionManager) {
-        this.state = positionManager.checkDirection(this.row, this.col, this.state);
+    public int move(LineManager lineManager) {
+        this.state = lineManager.nextDirection(this.current_row, this.current_col, this.state);
         if(state == 0){
             gotoDown();
         }
         followLine();
-        return this.col;
+        return this.current_col;
     }
 
     private void gotoDown() {
-        this.row = this.row + 1;
+        this.current_row = this.current_row + 1;
     }
 
     private void followLine() {
-        this.col = this.col + this.state;
+        this.current_col = this.current_col + this.state;
     }
 
     public boolean isRowExceedValue(int value) {
-        if(this.row > value)
+        validateValue(value);
+        if(this.current_row > value)
             return false;
-        return true;            
+        return true;
+    }
+
+    private void validateValue(int value) {
+        if(value < 0)
+            throw new IllegalArgumentException("value는 음수가 될 수 없습니다.");
     }
 }
