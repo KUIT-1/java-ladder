844de1c46b6759c81ee0024ab7bc0cdac0748b69
hhj <gudwn357@gmail.com>
2023-03-24 13:47:23 +0900

    test (LadderPositionFactoryTest) : ladder 필드 변수로 선언하여 중복 코드 제거 및 LadderPositionFactory 수정 사항 반영

--- src/test/java/ladder/LadderPositionFactoryTest.java
+++ src/test/java/ladder/LadderPositionFactoryTest.java
@@ -1,15 +1,23 @@
 package ladder;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class ApplicationInitTest {
+class LadderPositionFactoryTest {
+
+    static Ladder ladder;
+
+    @BeforeAll
+    static void setLadder() {
+        ladder = LadderPositionFactory.createLadder(4, 5);
+    }
 
     @Test
     public void createLadder() throws Exception {
         //given
-        Ladder ladder = ApplicationInit.createLadder(4, 5);
+        ladder = LadderPositionFactory.createLadder(4, 5);
 
         //then
         assertEquals(4, ladder.getRow());
@@ -20,16 +28,15 @@ class ApplicationInitTest {
     public void validateLadderException() throws Exception {
         //given
         /** row > 2*/
-        assertThrows(IllegalArgumentException.class, () -> ApplicationInit.createLadder(2, 5));
+        assertThrows(IllegalArgumentException.class, () -> LadderPositionFactory.createLadder(2, 5));
         /** numberOfPerson > 1*/
-        assertThrows(IllegalArgumentException.class, () -> ApplicationInit.createLadder(5, 1));
+        assertThrows(IllegalArgumentException.class, () -> LadderPositionFactory.createLadder(5, 1));
     }
 
     @Test
     public void createPosition() throws Exception {
         //given
-        Ladder ladder = ApplicationInit.createLadder(4, 5);
-        Position position = ApplicationInit.createPosition(1, 1, 1, 2);
+        Position position = LadderPositionFactory.createPosition(ladder, 1, 1, 2);
 
         //then
         assertEquals(1, position.getLeftPointXInt());
@@ -40,31 +47,24 @@ class ApplicationInitTest {
 
     @Test
     public void validatePositionException() throws Exception {
-        //given
-        Ladder ladder = ApplicationInit.createLadder(4, 5);
-
         //when
         /** Y 좌표값이 numberOfPerson을 넘어선 안됨 */
         assertThrows(IllegalArgumentException.class,
-                () -> ApplicationInit.createPosition(1, 10, 1, 11));
+                () -> LadderPositionFactory.createPosition(ladder, 10, 1, 11));
 
         /** X 좌표값이 row을 넘어선 안됨
          *  또한 마지막 row와 같아서도 안됨 */
         assertThrows(IllegalArgumentException.class,
-                () -> ApplicationInit.createPosition(10, 1, 10, 2));
-//        assertThrows(IllegalArgumentException.class,
-//                () -> ApplicationInit.createPosition(1, 1, 1, 2));
+                () -> LadderPositionFactory.createPosition(ladder, 1, 10, 2));
         assertThrows(IllegalArgumentException.class,
-                () -> ApplicationInit.createPosition(5, 2, 5, 2));
+                () -> LadderPositionFactory.createPosition(ladder, 2, 5, 2));
 
         /** rightPoint.Y > leftPoint.Y*/
         assertThrows(IllegalArgumentException.class,
-                () -> ApplicationInit.createPosition(1, 2, 1, 1));
+                () -> LadderPositionFactory.createPosition(ladder, 2, 1, 1));
         /** 두 좌표의 Y 값 차가 1이어야 함 */
         assertThrows(IllegalArgumentException.class,
-                () -> ApplicationInit.createPosition(1, 1, 1, 3));
-        /** 두 좌표의 X 값이 같아야 함 */
-        assertThrows(IllegalArgumentException.class,
-                () -> ApplicationInit.createPosition(1, 1, 2, 2));
+                () -> LadderPositionFactory.createPosition(ladder, 1, 1, 3));
+
     }
 }
