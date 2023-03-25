be7de4fb062d0b0ae899e46a6fb69805b05c1e36
hhj <gudwn357@gmail.com>
2023-03-25 23:55:19 +0900

    refactor (LadderGame) : LadderCreator를 의존하도록 변경

--- src/main/java/ladder/LadderGame.java
+++ src/main/java/ladder/LadderGame.java
@@ -1,33 +1,33 @@
 package ladder;
 
-import ladder.creator.SelfLadderCreator;
+import ladder.creator.LadderCreator;
 
 public class LadderGame {
 
-    SelfLadderCreator selfLadderCreator;
+    LadderCreator LadderCreator;
 
     public LadderGame(NumberOfRow row, NumberOfPerson numberOfPerson) {
-        selfLadderCreator = LadderPositionFactory.createLadderCreator(row, numberOfPerson);
+        LadderCreator = LadderPositionFactory.createSelfLadderCreator(row, numberOfPerson);
     }
 
     public void drawLine(int x, int leftY, int rightY) {
-        selfLadderCreator.drawLine(x,leftY,rightY);
+        LadderCreator.drawLine(x,leftY,rightY);
     }
 
     public int run(LadderNumber ladderNum) {
-        return LadderPositionFactory.createLadderRunner(selfLadderCreator).run(ladderNum);
+        return LadderPositionFactory.createLadderRunner(LadderCreator).run(ladderNum);
     }
 
     public int getNumberOfPerson() {
-        return selfLadderCreator.getRows()[0].getLength();
+        return LadderCreator.getRows()[0].getLength();
     }
 
     public int getRow() {
-        return selfLadderCreator.getRows().length;
+        return LadderCreator.getRows().length;
     }
 
     public Row[] getRowsForTest() {
-        return selfLadderCreator.getRows();
+        return LadderCreator.getRows();
     }
 
 }
