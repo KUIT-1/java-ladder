3a09b93460b2af06ccb76efe5110db1d43b73fde
hhj <gudwn357@gmail.com>
2023-03-25 22:45:26 +0900

    feat (Node) : Row.row 필드 Wrapper 클래스 생성

---
+++ src/main/java/ladder/Node.java
@@ -0,0 +1,35 @@
+package ladder;
+
+public class Node {
+
+    private Direction direction;
+
+    private Node(Direction direction) {
+        this.direction = direction;
+    }
+
+    public static Node createLeftNode() {
+        return new Node(Direction.LEFT);
+    }
+
+    public static Node createRightNode() {
+        return new Node(Direction.RIGHT);
+    }
+
+    public static Node createCenterNode() {
+        return new Node(Direction.CENTER);
+    }
+
+    public int getValue() {
+        return direction.getDirection();
+    }
+
+    public boolean isLeft() {
+        return direction.equals(Direction.LEFT);
+    }
+
+    public boolean isRight() {
+        return direction.equals(Direction.RIGHT);
+    }
+
+}
