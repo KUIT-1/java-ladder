be1e13651eb2eb5c7451a43d6c58523957e4acfa
hhj <gudwn357@gmail.com>
2023-03-26 14:56:08 +0900

    feat (WrapperFactory) : create

---
+++ src/main/java/ladder/WrapperFactory.java
@@ -0,0 +1,10 @@
+package ladder;
+
+import java.awt.*;
+
+public class WrapperFactory {
+
+    public static Position createPosition(LadderGame ladderGame, int x, int leftY, int rightY) {
+        return new Position(new Point(x, leftY), new Point(x, rightY), ladderGame);
+    }
+}
