8c0adb5aa90ee37250ed87d73c6c8b079a1aac59
hhj <gudwn357@gmail.com>
2023-03-27 00:20:30 +0900

    feat (LadderGameStarter) : create
    비지니스 로직 실행 (랜덤 사다리 그리기)

--- src/main/java/ladder/LadderGameStarter.java
+++ src/main/java/ladder/LadderGameStarter.java
@@ -1,8 +1,14 @@
 package ladder;
 
+import ladder.domain.wrapper.LadderNumber;
+import ladder.factory.LadderFactory;
+
 public class LadderGameStarter {
 
     public static void main(String[] args) {
 
+        LadderGame ladderGame = LadderFactory.createLadderGame(4, 5);
+        ladderGame.getLadderCreator().drawLine();
+        System.out.println("시작 0 = " + ladderGame.run(LadderNumber.createLadderNumber(0)));
     }
 }
