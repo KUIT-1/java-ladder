bfdc2fa4a0c954321282d7ce79c4fd77fe2bca06
hhj <gudwn357@gmail.com>
2023-03-26 22:38:55 +0900

    refactor (LadderNumber) : 검증 오류 수정

--- src/main/java/ladder/domain/wrapper/LadderNumber.java
+++ src/main/java/ladder/domain/wrapper/LadderNumber.java
@@ -14,13 +14,13 @@ public class LadderNumber {
     }
 
     private static void validateNumber(int number) {
-        if (!isPositiveNumber(number)) {
+        if (!isNaturalNumber(number)) {
             throw new IllegalArgumentException("0 이상이어야 합니다.");
         }
     }
 
-    private static boolean isPositiveNumber(int number) {
-        return number > 0;
+    private static boolean isNaturalNumber(int number) {
+        return number >= 0;
     }
 
     public void goRight() {
