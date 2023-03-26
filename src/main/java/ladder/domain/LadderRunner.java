5b87c1540e95600128e31a56ecd4f34bcfbb3cbe
hhj <gudwn357@gmail.com>
2023-03-27 00:18:16 +0900

    feat (LadderRunner) : add ladderViewer
    view() 메서드 호출

--- src/main/java/ladder/domain/LadderRunner.java
+++ src/main/java/ladder/domain/LadderRunner.java
@@ -1,6 +1,8 @@
 package ladder.domain;
 
+import ladder.domain.wrapper.CurrentPosition;
 import ladder.domain.wrapper.LadderNumber;
+import ladder.factory.LadderFactory;
 
 public class LadderRunner {
 
@@ -11,8 +13,13 @@ public class LadderRunner {
     }
 
     public int run(LadderNumber ladderNum) {
-        for (Row row : ladder.getRows()) {
-            row.nextPosition(ladderNum);
+        LadderViewer ladderViewer = LadderFactory.createLadderViewer(CurrentPosition.createCurrentPosition(ladderNum));
+
+        for (int i = 0; i < ladder.getRowSize(); i++) {
+            ladderViewer.getCurrentPosition().setX(i);
+            ladderViewer.view("BEFORE");
+            ladder.getRows()[i].nextPosition(ladderNum, ladderViewer.getCurrentPosition());
+            ladderViewer.view("AFTER");
         }
 
         return ladderNum.getNumber();
