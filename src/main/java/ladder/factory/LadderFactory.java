2c3a323f58f57b1cc685d5a4c314b3d4d8e43ff7
hhj <gudwn357@gmail.com>
2023-03-27 22:36:18 +0900

    refactor (LadderFactory) : createLadderGame()에서 LadderCreator 인스턴스 생성

--- src/main/java/ladder/factory/LadderFactory.java
+++ src/main/java/ladder/factory/LadderFactory.java
@@ -16,7 +16,14 @@ public class LadderFactory {
     private static Ladder ladder;
 
     public static LadderGame createLadderGame(int row, int numberOfPerson) {
-        return new LadderGame(NumberOfRow.createNumberOfRow(row), NumberOfPerson.createNumberOfPerson(numberOfPerson));
+        LadderCreator ladderCreator = LadderFactory.createSelfLadderCreator(
+                NumberOfRow.createNumberOfRow(row), NumberOfPerson.createNumberOfPerson(numberOfPerson));
+
+//        LadderCreator ladderCreator = LadderFactory.createRandomLadderCreator(
+//                NumberOfRow.createNumberOfRow(row), NumberOfPerson.createNumberOfPerson(numberOfPerson));
+
+        ladder = ladderCreator.getLadder();
+        return new LadderGame(ladderCreator, ladder);
     }
 
     public static LadderCreator createSelfLadderCreator(NumberOfRow row, NumberOfPerson numberOfPerson) {
