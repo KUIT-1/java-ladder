3b3243b31a1527d451d977b0cd18018c08bdc241
hhj <gudwn357@gmail.com>
2023-03-27 22:21:15 +0900

    test (LadderRunnerTest) : create
    selfLadderCreator 사용한 LadderRunner 테스트

---
+++ src/test/java/ladder/domain/LadderRunnerTest.java
@@ -0,0 +1,32 @@
+package ladder.domain;
+
+import ladder.domain.creator.LadderCreator;
+import ladder.domain.wrapper.LadderNumber;
+import ladder.domain.wrapper.NumberOfPerson;
+import ladder.domain.wrapper.NumberOfRow;
+import ladder.factory.LadderFactory;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LadderRunnerTest {
+
+    @Test
+    public void SelfLadderRunner() throws Exception {
+        //given
+        LadderCreator selfLadderCreator =
+                LadderFactory.createSelfLadderCreator(
+                        NumberOfRow.createNumberOfRow(4), NumberOfPerson.createNumberOfPerson(5));
+
+        LadderRunner ladderRunner = LadderFactory.createLadderRunner(selfLadderCreator);
+
+        //when
+        selfLadderCreator.drawLine(1, 1, 2);
+        selfLadderCreator.drawLine(2, 2, 3);
+
+        int result = ladderRunner.run(LadderNumber.createLadderNumber(1));
+
+        //then
+        assertEquals(3, result);
+    }
+}
