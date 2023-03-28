c02527cf02d31fb2a1b195ef0e9197b640a26c72
hhj <gudwn357@gmail.com>
2023-03-28 22:35:31 +0900

    refactor (LadderGame) : 코드 정리

--- src/main/java/ladder/LadderGame.java
+++ src/main/java/ladder/LadderGame.java
@@ -3,8 +3,6 @@ package ladder;
 import ladder.domain.Ladder;
 import ladder.domain.creator.LadderCreator;
 import ladder.domain.wrapper.LadderNumber;
-import ladder.domain.wrapper.NumberOfPerson;
-import ladder.domain.wrapper.NumberOfRow;
 import ladder.factory.LadderFactory;
 
 public class LadderGame {
@@ -19,14 +17,6 @@ public class LadderGame {
 
     public int run(LadderNumber ladderNum) {
         return LadderFactory.createLadderRunner(ladderCreator).run(ladderNum);
-    }
-
-    public int getRowSize() {
-        return ladder.getRowSize();
-    }
-
-    public int getNumberOfPersonSize() {
-        return ladder.getNumberOfPersonSize();
     }
 
     public Ladder getLadder() {
