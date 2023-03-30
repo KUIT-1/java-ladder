aabb392e37a02fb4436aebfea7b41c79f3f9d2ad
kwiyoon <jiyun0245@gmail.com>
2023-03-31 06:52:39 +0900

    feat: Create class that wraps row and col

--- src/main/java/ladder/random/Point.java
+++ src/main/java/ladder/random/Point.java
@@ -1,2 +1,47 @@
-package ladder.random;public class Point {
+package ladder.random;
+
+import ladder.NaturalNumber;
+import ladder.Position;
+
+public class Point{
+
+    private Position row;
+    private Position col;
+
+    private Point(Position row, Position col){
+        this.row = row;
+        this.col = col;
+    }
+    public static Point createPoint(Position row, Position col){
+        return new Point(row, col);
+    }
+
+    public int getRow(){
+        return row.getPosition();
+    }
+
+    public int getCol(){
+        return col.getPosition();
+    }
+
+    private boolean IsSamePoint(Point other){
+        if(getRow() == other.getRow()
+                && getCol() == other.getCol())
+            return true;
+        return false;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if(o instanceof Point){
+            Point other = (Point) o;
+            return IsSamePoint(other);
+        }
+        return false;
+    }
+    @Override
+    public int hashCode() {
+        return getRow() * getCol();
+    }
+
 }
