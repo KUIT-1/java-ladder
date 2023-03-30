c178cb1b6ebe841b5e4c1d3d6aa70da5359b171d
kwiyoon <jiyun0245@gmail.com>
2023-03-31 06:53:41 +0900

    feat: Create random lines on the ladder
    HashSet<Point>를 이용하여 중복을 확인하고, line을 추가
    Override - Point의 hashcode와 equals

--- src/test/java/ladder/PositionTest.java
+++ src/test/java/ladder/PositionTest.java
@@ -10,14 +10,14 @@ import static org.junit.jupiter.api.Assertions.*;
 public class PositionTest {
     @Test
     @DisplayName("Position 생성자")
-    void createPositionTest(){
+    void When_createPositionByVal_Expect_getPositionIsVal(){
         Position position = createPosition(3);
         assertEquals(3, position.getPosition());
     }
 
     @Test
     @DisplayName("Position의 다음 위치")
-    void nextPositionTest(){
+    void When_CallNextPositionByA_Expect_ReturnAPlusOne(){
         Position position = createPosition(3);
         Position nextPosition = position.nextPosition();
         assertEquals(4, nextPosition.getPosition());
