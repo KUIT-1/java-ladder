59f39d34d29d90c12186efb663fa631ea95a1174
yoon-gang00 <sgi07151@naver.com>
2023-03-31 22:34:12 +0900

    refactoring : Ladder.java

--- src/main/java/ladder/Ladder.java
+++ src/main/java/ladder/Ladder.java
@@ -22,31 +22,31 @@ public class Ladder {
         rows[rowNumber - 1].drawLine(position);
     }
 
-    public int run(int startPosition) {
-        int currentPosition = startPosition;
-        for (Row row : rows) {
-            if (currentPosition > 1 && row.isLineDrawn(currentPosition - 1)) {
-                currentPosition--;
-            } else if (currentPosition < row.getNumberOfPerson() && row.isLineDrawn(currentPosition)) {
-                currentPosition++;
-            }
-        }
-        return currentPosition;
-    }
-    private Direction getDirection(Row row, int position) {
-        if (position <= 1) {
-            return Direction.RIGHT;
-        } else if (position >= row.getNumberOfPerson()) {
-            return Direction.LEFT;
-        } else if (row.isLineDrawn(position - 1)) {
-            return Direction.LEFT;
-        } else {
-            return Direction.RIGHT;
-        }
-    }
-    public void runAll() {
-        for (int i = 1; i <= rows[0].getNumberOfPerson(); i++) {
-            System.out.println("시작 지점: " + i + " -> " + run(i));
-        }
-    }
+//    public int run(int startPosition) {
+//        int currentPosition = startPosition;
+//        for (Row row : rows) {
+//            if (currentPosition > 1 && row.isLineDrawn(currentPosition - 1)) {
+//                currentPosition--;
+//            } else if (currentPosition < row.getNumberOfPerson() && row.isLineDrawn(currentPosition)) {
+//                currentPosition++;
+//            }
+//        }
+//        return currentPosition;
+//    }
+//    private Direction getDirection(Row row, int position) {
+//        if (position <= 1) {
+//            return Direction.RIGHT;
+//        } else if (position >= row.getNumberOfPerson()) {
+//            return Direction.LEFT;
+//        } else if (row.isLineDrawn(position - 1)) {
+//            return Direction.LEFT;
+//        } else {
+//            return Direction.RIGHT;
+//        }
+//    }
+//    public void runAll() {
+//        for (int i = 1; i <= rows[0].getNumberOfPerson(); i++) {
+//            System.out.println("시작 지점: " + i + " -> " + run(i));
+//        }
+//    }
 }
