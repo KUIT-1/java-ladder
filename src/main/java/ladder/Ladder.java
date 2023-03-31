001a2dc392dac4489660c888e731ec39555964f5
hyuja <wjsgmlwl61@gmail.com>
2023-03-31 13:29:17 +0900

    refactor :  numOfPeople와 ladderHeight를 의미하는 변수를 final로 변경하여 불변성을 보장 refactor : ladder 배열에서 가로 인덱스와 세로 인덱스를 바꿔서 가로 줄과 세로 줄의 정보를 명확하게 구분 refactor : drawLine 메소드에서는 랜덤한 값을 생성하는 부분을 moveRightOrLeft메소드로 추출 refactor : run 메소드에서 numOfPeople - 1번 drawLne 메소드를 호출하는 부분은 바깥쪽 루프에서 수행되므로 중복 호출을 개선
    refactor : drawLine 오타 수정

--- src/main/java/ladder/Ladder.java
+++ src/main/java/ladder/Ladder.java
@@ -3,44 +3,39 @@ package ladder;
 import java.util.Random;
 
 public class Ladder {
-
-
-    private int numOfPeople;
-    private int ladderHeight;
+    private final int numOfPeople;
+    private final int ladderHeight;
     private int[][] ladder;
 
     public Ladder(int numOfPeople, int ladderHeight) {
         this.numOfPeople = numOfPeople;
         this.ladderHeight = ladderHeight;
-        ladder = new int[ladderHeight][numOfPeople - 1];
-
+        ladder = new int[numOfPeople - 1][ladderHeight];
     }
 
-    private void drawLne(int position) {
+    private int moveRightOrLeft(int currentPosition) {
+        return new Random().nextBoolean() ? currentPosition + 1 : currentPosition - 1;
+    }
 
-        Random random = new Random();
+    private void drawLine(int lineIndex) {
+        int position = lineIndex;
         for (int i = 0; i < ladderHeight; i++) {
-            if (random.nextBoolean()) {
-                position++;
-            } else if (position > 0) {
-                position--;
-            }
-            ladder[i][position] = 1;
+            position = moveRightOrLeft(position);
+            ladder[lineIndex][i] = position;
         }
     }
 
     public int run(int selectedLine) {
-        for (int i = 0; i < numOfPeople - 1; i++) {
-            drawLne(i);
+        for (int i = 0; i < ladder.length; i++) {
+            drawLine(i);
         }
         int result = selectedLine;
         for (int i = 0; i < ladderHeight; i++) {
-            if (ladder[i][result] == 1) {
-                result++;
-            } else if (result > 0 && ladder[i][result - 1] == 1) {
-                result--;
+            int currentLine = result - 1;
+            if (currentLine >= 0 && currentLine < ladder.length && ladder[currentLine][i] != i) {
+                result = ladder[currentLine][i];
             }
         }
-        return result + 1; //사다리번호는 1부터시작하므로 +1
+        return result;
     }
 }
